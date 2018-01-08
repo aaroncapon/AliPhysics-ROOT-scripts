50f213c525617444409e20eec6e82426c0108be2
aaroncapon <aaroncapon@cern.ch>
2018-01-08 11:49:48 +0100

    Includes fast set cuts

--- commonAnalysisVariables.C
+++ commonAnalysisVariables.C
@@ -1,16 +1,25 @@
 //A file to store common variables used throughout analysis
 struct analysisCuts16q{
 	
-	//PID cuts
-	const Float_t MVAcut = 0.05;
-	//cent cuts (same as Scheids DCA analysis cuts)
+	//------------PID cuts-------------------//
+	//MVA cuts
+	const Float_t MVAcut_cent = 0.05;
+	const Float_t MVAcut_fast = 0.1;
+
+	//Standard Cuts
+	//Common cuts between data sets
 	const Float_t minEsigITS = -3;
 	const Float_t maxEsigITS = 1;
-	const Float_t minEsigTPC = -1.5;
-	const Float_t maxEsigTPC = 4;
-	const Float_t minPsigTPC = 3.5;
 	const Float_t minEsigTOF = -3;
 	const Float_t maxEsigTOF = 3;
+	//cent specific PID cuts
+	const Float_t minEsigTPC_cent = -1.5;
+	const Float_t maxEsigTPC_cent = 4;
+	const Float_t minPsigTPC_cent = 3.5;
+	//fast speicific PID cuts
+	const Float_t minEsigTPC_cent = -3;
+	const Float_t maxEsigTPC_cent = 3;
+	const Float_t minPsigTPC_cent = 4;
 
 
 	//--------- Track cuts --------------------//
@@ -20,13 +29,17 @@ struct analysisCuts16q{
 	const Float_t maxTPCcrossFindable = 1.1;
 	const Float_t maxFracTPCshared = 0.4;
 
-	const Float_t minITShits = 4;
+	//ITS cuts are implented as less than cuts
+	const Float_t minITShits_cent = 4;
+	const Float_t minITShits_fast = 3;
+
 	const Float_t maxITSchi2 = 4.5;
 	const Float_t maxfITSshared = 0.01;
 
 	const Float_t maxDCAz = 3;
 	const Float_t maxDCAxy = 1;
 
+	const Bool_t reqSPD = kTRUE;
 
 	//--------- Pair cuts --------------------//
 	const Float_t minPhiVmassCut = 0.02; //Exclude smaller than
@@ -37,6 +50,6 @@ struct analysisCuts16q{
 	//Float_t minInvMassPrefilter = 0.05;
 	//Float_t minOpAnglePrefilter = 0.05;
 
-	const Float_t minPhiVmassCutPre = 0.02; //Exclude smaller than
+	const Float_t minPhiVmassPre = 0.02; //Exclude smaller than
 	const Float_t maxPhiVPre = (3/4.)*TMath::Pi(); //Exclude greater than
 };
