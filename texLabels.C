29ad980bc7b6e13f7951a0a04d6d3a37906475f0
aaroncapon <aaroncapon@cern.ch>
2018-04-24 10:14:05 +0200

    Edit TLatex title option added
    Also formatting..

--- texLabels.C
+++ texLabels.C
@@ -1,40 +1,41 @@
 //Common TLatex objects
-TLatex* getTexTitle(Float_t xPos, Float_t yPos, Float_t textSize = 0.04){
+TLatex* getTexTitle(Float_t xPos, Float_t yPos, TString title = "", Float_t textSize = 0.04){
 
-    TLatex* tex = new TLatex(xPos, yPos, "ALICE Work in Progress");
-    tex->SetNDC();
-    tex->SetTextSize(textSize);
-    tex->SetTextFont(42);
+	
+	TLatex* tex = new TLatex(xPos, yPos, title);
+	tex->SetNDC();
+	tex->SetTextSize(textSize);
+	tex->SetTextFont(42);
 
-    return tex;
+	return tex;
 }
 TLatex* getTexSystem(Float_t xPos, Float_t yPos, TString multRange = "", Bool_t isMC = kFALSE, Float_t textSize = 0.03){
 
-    TString description = "p-Pb, #sqrt{#it{s}_{NN}} = 5.02 TeV";
-		if(multRange != "" && multRange != "MB"){
-			description += ", " + multRange + " %";
-		}
-		else if(multRange == "MB"){
-			description += ", MB";
-		}
-    if(isMC == kTRUE){
-       description += ", DPMJET";
-    }
-    TLatex* tex = new TLatex(xPos, yPos, description);
-    tex->SetNDC();
-    tex->SetTextSize(textSize);
-    tex->SetTextFont(42);
+	TString description = "p-Pb, #sqrt{#it{s}_{NN}} = 5.02 TeV";
+	if(multRange != "" && multRange != "MB"){
+		description += ", " + multRange + " %";
+	}
+	else if(multRange == "MB"){
+		description += ", MB";
+	}
+	if(isMC == kTRUE){
+		 description += ", DPMJET";
+	}
+	TLatex* tex = new TLatex(xPos, yPos, description);
+	tex->SetNDC();
+	tex->SetTextSize(textSize);
+	tex->SetTextFont(42);
 
-    return tex;
+	return tex;
 }
 TLatex* getTexKinematics(Float_t xPos, Float_t yPos, Float_t textSize = 0.02){
 
-    TLatex* tex = new TLatex(xPos, yPos, "0.2 < #it{p}_{T} < 10 GeV/#it{c}, |#eta| < 0.8");
-    tex->SetNDC();
-    tex->SetTextSize(textSize);
-    tex->SetTextFont(42);
+	TLatex* tex = new TLatex(xPos, yPos, "0.2 < #it{p}_{T} < 10 GeV/#it{c}, |#eta| < 0.8");
+	tex->SetNDC();
+	tex->SetTextSize(textSize);
+	tex->SetTextFont(42);
 
-    return tex;
+	return tex;
 }
 TLatex* getTexPairMom(Float_t xPos, Float_t yPos, Float_t textSize = 0.02, TString maxPt = "10", TString minPt = "0"){
 
